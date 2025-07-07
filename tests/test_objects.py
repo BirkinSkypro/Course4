fa46b6114a207b924cd951c2ed21bfe3f4bfdf43
Vadim V.Birkin <warpson7pixel@gmail.com>
2025-07-07 18:29:19 +0700

    15.1 last

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -39,6 +39,7 @@ def test_Product_add(Product_Qq):
         "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
     )
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    assert (product1+product2) == 2580000.0
     assert product1.__add__(product2) == 2580000.0
 
 
